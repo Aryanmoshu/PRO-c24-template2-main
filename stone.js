37e5a77e70ea7dfc08bf41c91c2571bcee94da62
Aryanmoshu <aryansharma.him@gmail.com>
2021-06-28 16:00:42 +0530

    first commit

---
+++ stone.js
@@ -0,0 +1,25 @@
+class Stone{
+    constructor(x,y,width,height){
+        var option={
+            'restitution':0.5,
+            'friction':0.8,
+            'density':10
+       }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+    push()
+        translate(pos.x,pos.y);
+        rotate(angle)
+            stroke("black");
+            fill('grey');
+            rect(0,0,this.width,this.height);
+    pop();
+    }
+   
+}
